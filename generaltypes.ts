fa3059ca844357263d94d9a2011d00478bcc4f14
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-02-10 16:29:02 -0500

    added the library files

---
+++ generaltypes.ts
@@ -0,0 +1,3 @@
+export type AnyDict = {
+    [key: string]: any
+}
---
+++ kiosktsapplib.ts
@@ -0,0 +1,6 @@
+export * from "./devkioskapi"
+export * from "./kioskapp"
+export * from "./kioskapi"
+export * from "./kioskappcomponent"
+export * from "./kioskdevstarter"
+
